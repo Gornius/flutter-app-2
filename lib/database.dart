b0cb20f63d1eda37122758c0a7d25d65511ca732
Rafał Cisek <rafal.cisek121@gmail.com>
2022-06-06 16:51:06 +0200

    init

---
+++ lib/database.dart
@@ -0,0 +1,104 @@
+import 'dart:async';
+import 'package:path/path.dart' as Path;
+import 'package:sqflite/sqflite.dart';
+
+class Phone {
+  int id;
+  String name;
+  String manufacturer;
+  String model;
+  String softwareVersion;
+  String phoneAvatar;
+
+  Phone({
+    required this.id,
+    required this.name,
+    required this.model,
+    required this.manufacturer,
+    required this.softwareVersion,
+    required this.phoneAvatar,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'manufacturer': manufacturer,
+      'model': model,
+      'softwareVersion': softwareVersion,
+      'phoneAvatar': phoneAvatar,
+    };
+  }
+
+  Map<String, dynamic> toMapNoId() {
+    var newMap = toMap();
+    newMap.remove('id');
+
+    return newMap;
+  }
+
+  @override
+  String toString() {
+    return toMap().toString();
+  }
+}
+
+class PhoneDatabase {
+  static const dbFileName = 'phone_database.db';
+  static const dbVersion = 1;
+
+  static const tableName = 'phones';
+  static const idColumn = 'id';
+  static const nameColumn = 'name';
+  static const manufacturerColumn = 'manufacturer';
+  static const modelColumn = 'model';
+  static const softwareVersionColumn = 'softwareVersion';
+  static const phoneAvatarColumn = 'phoneAvatar';
+
+  static const createDbSql = 'CREATE TABLE $tableName'
+      '($idColumn INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+      '$nameColumn TEXT,'
+      '$manufacturerColumn TEXT,'
+      '$modelColumn TEXT,'
+      '$softwareVersionColumn TEXT,'
+      '$phoneAvatarColumn TEXT);';
+
+  static Future<Database> openPhoneDatabase() async {
+    return openDatabase(
+      Path.join(await getDatabasesPath(), dbFileName),
+      onCreate: (db, version) {
+        return db.execute(createDbSql);
+      },
+      version: dbVersion,
+    );
+  }
+
+  static Future<void> resetDatabase() async {
+    databaseFactory.deleteDatabase(dbFileName);
+  }
+
+  static Future<List<Phone>> getPhones() async {
+    final phoneDatabase = await openPhoneDatabase();
+
+    final List<Map<String, dynamic>> phoneMapList =
+        await phoneDatabase.query(tableName);
+
+    return List.generate(phoneMapList.length, (i) {
+      return Phone(
+          id: phoneMapList[i][idColumn],
+          name: phoneMapList[i][nameColumn],
+          model: phoneMapList[i][modelColumn],
+          manufacturer: phoneMapList[i][manufacturerColumn],
+          softwareVersion: phoneMapList[i][softwareVersionColumn],
+          phoneAvatar: phoneMapList[i][phoneAvatarColumn]);
+    });
+  }
+
+  static Future<int> insertPhone(Phone phone) async {
+    final phoneDatabase = await openPhoneDatabase();
+    final newItemId = await phoneDatabase.insert(tableName, phone.toMapNoId(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+
+    return newItemId;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'database.dart';
+import 'dart:developer';
+
+void main() {
+  runApp(const MyApp());
+  inspect(PhoneDatabase.getPhones());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: const ColorScheme.dark().copyWith(
+          secondary: Colors.purple,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<Phone> _phoneList;
+  bool _isLoadingData = true;
+
+  void _loadPhoneList() async {
+    final phoneList = await PhoneDatabase.getPhones();
+    setState(() {
+      _phoneList = phoneList;
+      _isLoadingData = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPhoneList();
+  }
+
+  Widget _buildPhoneList() {
+    return ListView.builder(
+      itemCount: _phoneList.length,
+      itemBuilder: (context, i) => Card(
+        color: Colors.lightBlueAccent,
+        margin: const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+        child: ListTile(
+          title: Text(_phoneList[i].name),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Phone Database'),
+      ),
+      body: _isLoadingData
+          ? const Center(child: CircularProgressIndicator())
+          : _buildPhoneList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showPhoneForm(null),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  final TextEditingController _phoneNameFieldController =
+      TextEditingController();
+
+  _showPhoneForm(int? phoneId) {
+    if (phoneId != null) {
+      final existingPhone =
+          _phoneList.firstWhere((element) => element.id == phoneId);
+      _phoneNameFieldController.text = existingPhone.name;
+    }
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
